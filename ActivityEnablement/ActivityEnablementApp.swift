4f2712dd684e2169fc6da7765fee12d107fd4995
Gene Bogdanovich <genebogdanovich@gmail.com>
2025-05-26 09:28:13 +0300

    Initial Commit

---
+++ ActivityEnablement/ActivityEnablementApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityEnablementApp.swift
+//  ActivityEnablement
+//
+//  Created by Gene Bogdanovich on 26.05.25.
+//
+
+import SwiftUI
+
+@main
+struct ActivityEnablementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
